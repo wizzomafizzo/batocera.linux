89b67d4d46022a2e735891c7b4dd73bf49db729f
Romain TISSERAND <romain.tisserand@gmail.com>
2021-05-16 19:24:39 +0200

    WIP SBC Wayland

--- package/batocera/emulators/retroarch/retroarch/retroarch.mk
+++ package/batocera/emulators/retroarch/retroarch/retroarch.mk
@@ -3,8 +3,8 @@
 # retroarch
 #
 ################################################################################
-# Version.: Release on Mar 29, 2021
-RETROARCH_VERSION = v1.9.1
+# Version.: Release on May 16, 2021
+RETROARCH_VERSION = v1.9.3
 RETROARCH_SITE = $(call github,libretro,RetroArch,$(RETROARCH_VERSION))
 RETROARCH_LICENSE = GPLv3+
 RETROARCH_DEPENDENCIES = host-pkgconf dejavu retroarch-assets flac 
